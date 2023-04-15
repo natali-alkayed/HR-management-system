c384a456ba67a83ad422846176c570996efc8633
natali-alkayed <nataly2000alkayed@gmail.com>
2023-04-15 14:59:31 +0000

    calculations done

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 'use strict';
 let myForm =document.getElementById("employeesForm");
-let infoSection = document.getElementById('cardSection');
 /////////////////////////////////////////////////////////////////////////////////////
 let informations = [];
 function EmployeeInfo (FullName,Department,Level,ImageURL)
@@ -28,7 +27,7 @@ let newEmployee=new EmployeeInfo(fullName,department,level,image);
 newEmployee.randomID();
 newEmployee.Salary(level);
 newEmployee.render();
-
+saveData(informations);
 }
 ///////////////////////////////////////////////////////////////////////////////////////
 let EmployeeID=1000;
@@ -74,72 +73,36 @@ EmployeeInfo.prototype.render=function()
    cardEl.appendChild(trEl);
 
    let imgEl = document.createElement('img');
-    imgEl.src = this.ImageURL;
-    trEl.appendChild(imgEl);
-  
-    let NameEl= document.createElement('p');
-    NameEl.textContent =`Name: ${this.FullName} - 
-    ID:${this.EmployeeID}
-    -Department: ${this.Department} 
-    - Level: ${this.Level} 
-    -Salary: ${this.salary}`;
-    trEl.appendChild(NameEl);
-/////////
-let cardEl2 = document.getElementById('Marketing');
-let trEl2 = document.createElement('div');
-cardEl2.appendChild(trEl2);
+   imgEl.src = this.ImageURL;
+   trEl.appendChild(imgEl);
 
-let imgEl2 = document.createElement('img');
- imgEl2.src = this.ImageURL;
- trEl2.appendChild(imgEl2);
+   let NameEl= document.createElement('p');
+   NameEl.textContent =`Name: ${this.FullName}`
+   trEl.appendChild(NameEl);
 
- let NameEl2= document.createElement('p');
- NameEl2.textContent =`Name: ${this.FullName}  - 
- ID:${this.EmployeeID}
- -Department: ${this.Department} 
- - Level: ${this.Level} 
- -Salary: ${this.salary}`;
- trEl2.appendChild(NameEl2);
-/////////
-let cardEl3 = document.getElementById('Administration');
-let trEl3 = document.createElement('div');
-cardEl3.appendChild(trEl3);
+   let IDEl= document.createElement('p');
+   IDEl.textContent =`ID: ${this.EmployeeID}`
+   trEl.appendChild(IDEl);
 
-let imgEl3 = document.createElement('img');
- imgEl3.src = this.ImageURL;
- trEl3.appendChild(imgEl3);
+   let DepEl= document.createElement('p');
+   DepEl.textContent =`Department: ${this.Department}`
+   trEl.appendChild(DepEl);
 
- let NameEl3= document.createElement('p');
- NameEl3.textContent =`Name: ${this.FullName}  - 
- ID:${this.EmployeeID}
- -Department: ${this.Department} 
- - Level: ${this.Level} 
- -Salary: ${this.salary}`;
- /////////
- let cardEl4 = document.getElementById('Development');
- let trEl4 = document.createElement('div');
- cardEl4.appendChild(trEl4);
- 
- let imgEl4 = document.createElement('img');
-  imgEl4.src = this.ImageURL;
-  trEl4.appendChild(imgEl4);
- 
-  let NameEl4= document.createElement('p');
-  NameEl4.textContent =`Name: ${this.FullName}  - 
-  ID:${this.EmployeeID}
-  -Department: ${this.Department} 
-  - Level: ${this.Level} 
-  -Salary: ${this.salary}`;
-  
+   let levEl= document.createElement('p');
+   levEl.textContent =`Level: ${this.Level}`
+   trEl.appendChild(levEl);
+
+   let salEl= document.createElement('p');
+   salEl.textContent =`Salary: ${this.salary}`
+   trEl.appendChild(salEl);
 }
 
-
 ///////////////////////////////////////////////////////////////////////////////////////////////
-for (let i = 0; i < informations.length; i++) {
+/*for (let i = 0; i < informations.length; i++) {
     informations[i].randomID();
     informations[i].Salary(level);
     informations[i].render();
-}
+}*/
 ///////////////////////////////////////////////////////////////////////////////////////////////
 function saveData(data) {
    let stringifyData = JSON.stringify(data);
@@ -163,40 +126,40 @@ function getData() {
 let employee1=new EmployeeInfo("Ghazi Samer","Administration","Senior","./assets/employee1.png");
 employee1.Salary(employee1.Level);
 employee1.randomID(employee1.Level);
-employee1.render(employee1.Level);
+employee1.render();
 
 
 let employee2=new EmployeeInfo("Lana Ali","Finance","Senior","./assets/employee2.png");
 employee2.Salary(employee2.Level);
 employee2.randomID(employee2.Level);
-employee2.render(employee2.Level);
+employee2.render();
 
 let employee3=new EmployeeInfo("Tamara Ayoub","Marketing","Senior","./assets/employee2.png");
 employee3.Salary(employee3.Level);
 employee3.randomID(employee3.Level);
-employee3.render(employee3.Level);
+employee3.render();
 
 let employee4=new EmployeeInfo("Safi Walid","Administration","Mid-Senior","./assets/employee1.png");
 employee4.Salary(employee4.Level);
 employee4.randomID(employee4.Level);
-employee4.render(employee4.Level);
+employee4.render();
 
 let employee5=new EmployeeInfo("Omar Zaid","Development","Senior","./assets/employee1.png");
 employee5.Salary(employee5.Level);
 employee5.randomID(employee5.Level);
-employee5.render(employee5.Level);
+employee5.render();
 
 let employee6=new EmployeeInfo("Rana Saleh","Development","Junior","./assets/employee2.png");
 employee6.Salary(employee6.Level);
 employee6.randomID(employee6.Level);
-employee6.render(employee6.Level);
+employee6.render();
 
 let employee7=new EmployeeInfo("Hadi Ahmad","Finance","Mid-Senior","./assets/employee1.png");
 employee7.Salary(employee7.Level);
 employee7.randomID(employee7.Level);
-employee7.render(employee7.Level);
+employee7.render();
 /////////////////////////////////////////////////////////////////////////////////////////////////////
 
-console.log(informations);
 saveData(informations);
+console.log(informations);
 
